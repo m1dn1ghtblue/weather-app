90e8b40ed07baaa8baedbc9b867194c65155e57a
Ilya <kotyrov.1310@gmail.com>
2023-04-07 16:25:21 +0200

    implement data fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,48 @@
+'use strict';
+
 import autosizeInput from 'autosize-input';
 import '../styles/index.scss';
+import getWeatherData from './weatherData';
 
+const locationForm = document.getElementById('location-form');
 const locationInput = document.getElementById('location-input');
+const locationDetailsLabel = document.getElementById('location-details');
 
+// third-party library function to make text input resize to content
 autosizeInput(locationInput);
+
 locationInput.focus();
+
+locationForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	updateData(locationInput.value);
+});
+
+locationInput.addEventListener('focusout', () => updateData(locationInput.value));
+
+function updateData(location) {
+	clearError();
+
+	if (!location) {
+		return;
+	}
+
+	getWeatherData(location)
+		.then(displayData)
+		.catch((error) => {
+			console.error(`Failed to update data: ${error}`);
+			showError();
+		});
+}
+
+function displayData(data) {
+	console.log(data);
+}
+
+function showError() {
+	locationInput.classList.add('invalid');
+}
+
+function clearError() {
+	locationInput.classList.remove('invalid');
+}
---
+++ src/scripts/weatherData.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const API_KEY = '18d0a9f1ed314c8e8cc120921230304';
+
+export default async function getWeatherData(location) {
+	try {
+		const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&aqi=no&q=${location}`);
+		if (!response.ok) {
+			throw Error(`Response status: ${response.status}`);
+		}
+
+		const jsonData = await response.json();
+
+		return Promise.resolve(jsonData);
+	} catch (error) {
+		return Promise.reject(`Failed to get data for location '${location}': ${error}`);
+	}
+}
