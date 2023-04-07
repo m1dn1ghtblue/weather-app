82155a6074f83459178231c851acbe139f9a0ab0
Ilya <kotyrov.1310@gmail.com>
2023-04-07 15:46:52 +0200

    add location input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
+import autosizeInput from 'autosize-input';
 import '../styles/index.scss';
+
+const locationInput = document.getElementById('location-input');
+
+autosizeInput(locationInput);
+locationInput.focus();
