7c7b298b9bb29f4a4f94602121455ed96dc5782a
Ilya <kotyrov.1310@gmail.com>
2023-04-08 17:58:05 +0200

    display cuurent weather details

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -39,6 +39,7 @@ function displayData(data) {
 	console.log(data);
 	updateLocationDetails(data.location);
 	updateCurrentWeatherInfo(data.current);
+	updateCurrentWeatherDetails(data.current);
 }
 
 function showError() {
@@ -73,4 +74,29 @@ function updateCurrentWeatherInfo(weatherData) {
 	feelsLikeLabel.textContent = weatherData.feelslike_c;
 	conditionLabel.textContent = weatherData.condition.text;
 	conditionIcon.innerHTML = getIcon(weatherData.condition.text);
+}
+
+const aqiStatus = {
+	1: 'Good',
+	2: 'Moderate',
+	3: 'Unhealthy for sensitive group',
+	4: 'Unhealthy',
+	5: 'Very unhealthy',
+	6: 'Hazardous',
+};
+
+function updateCurrentWeatherDetails(weatherData) {
+	const windSpeedLabel = document.getElementById('details-wind-speed');
+	const windDirLabel = document.getElementById('details-wind-dir');
+	const humidityLabel = document.getElementById('details-humidity');
+	const cloudLabel = document.getElementById('details-cloud');
+	const pressureLabel = document.getElementById('details-pressure');
+	const aqiLabel = document.getElementById('details-aqi');
+
+	windSpeedLabel.textContent = `${weatherData.wind_kph}kph`;
+	windDirLabel.textContent = `${weatherData.wind_dir} (${weatherData.wind_degree}\xBA)`;
+	humidityLabel.textContent = `${weatherData.humidity}%`;
+	cloudLabel.textContent = `${weatherData.cloud}%`;
+	pressureLabel.textContent = `${weatherData.pressure_mb}mb`;
+	aqiLabel.textContent = aqiStatus[weatherData.air_quality['us-epa-index']];
 }
--- src/scripts/weatherData.js
+++ src/scripts/weatherData.js
@@ -4,7 +4,7 @@ const API_KEY = '18d0a9f1ed314c8e8cc120921230304';
 
 export default async function getWeatherData(location) {
 	try {
-		const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&aqi=no&q=${location}`);
+		const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&aqi=yes&q=${location}`);
 		if (!response.ok) {
 			throw Error(`Response status: ${response.status}`);
 		}
